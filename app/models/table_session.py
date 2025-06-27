8b62a532966c312ee1e7a199184ef5f25885581d
Delcio Agostinho <64657972+dElCIoGio@users.noreply.github.com>
2025-06-28 00:49:20 +0100

    Broadcast table session updates

--- app/models/table_session.py
+++ app/models/table_session.py
@@ -1,8 +1,19 @@
+from typing import Dict, Any, Optional
+import json
+
 from app.db.crud import MongoCrud
 from app.schema import table_session as table_session_schema
+from app.services.websocket_manager import get_websocket_manger
 
 
 class TableSessionModel(MongoCrud[table_session_schema.TableSessionDocument]):
-
     def __init__(self):
         super().__init__(table_session_schema.TableSessionDocument)
+
+    async def update(self, _id: str, data: Dict[str, Any]) -> Optional[table_session_schema.TableSessionDocument]:
+        updated = await super().update(_id, data)
+        if updated and ("orders" in data or "status" in data):
+            websocket_manager = get_websocket_manger()
+            session_data = json.dumps(updated.to_response().model_dump())
+            await websocket_manager.broadcast(session_data, f"{updated.restaurant_id}/table-status")
+        return updated
