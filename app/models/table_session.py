ac7eda0fb79641c33c036449582fd8f22a317021
Delcio Agostinho <64657972+dElCIoGio@users.noreply.github.com>
2025-08-04 19:36:56 +0100

    Broadcast assistance updates on needs assistance changes

--- app/models/table_session.py
+++ app/models/table_session.py
@@ -10,11 +10,22 @@ class TableSessionModel(MongoCrud[table_session_schema.TableSessionDocument]):
     def __init__(self):
         super().__init__(table_session_schema.TableSessionDocument)
 
-    async def update(self, _id: str, data: Dict[str, Any]) -> Optional[table_session_schema.TableSessionDocument]:
+    async def update(
+        self, _id: str, data: Dict[str, Any]
+    ) -> Optional[table_session_schema.TableSessionDocument]:
         updated = await super().update(_id, data)
-        if updated and ("orders" in data or "status" in data or "needsAssistance" in data):
+        if updated and (
+            "orders" in data or "status" in data or "needsAssistance" in data
+        ):
             websocket_manager = get_websocket_manger()
             json_session = updated.to_response().model_dump(by_alias=True)
             session_data = json.dumps(json_session)
-            await websocket_manager.broadcast(session_data, f"{str(updated.restaurant_id)}/session-status")
+            await websocket_manager.broadcast(
+                session_data, f"{str(updated.restaurant_id)}/session-status"
+            )
+
+            if "needsAssistance" in data:
+                await websocket_manager.broadcast(
+                    session_data, f"{str(updated.restaurant_id)}/assistance"
+                )
         return updated
